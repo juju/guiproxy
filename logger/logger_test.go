8a86f9514ca2b3276e28ea748e6c0630b51bd87e
Francesco Banconi <francesco.banconi@canonical.com>
2017-12-22 12:40:46 +0100

    Update tests to replace manual patch functions with qt.C.Patch.

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -12,8 +12,8 @@ import (
 
 func TestPrint(t *testing.T) {
 	c := qt.New(t)
-	restore, getOutput := patchLogPrintln()
-	defer restore()
+	defer c.Cleanup()
+	getOutput := patchLogPrintln(c)
 	l := logger.New()
 	l.Print("these are the voyages")
 	c.Assert(getOutput(), qt.Equals, "these are the voyages\n")
@@ -21,8 +21,8 @@ func TestPrint(t *testing.T) {
 
 func TestModifiers(t *testing.T) {
 	c := qt.New(t)
-	restore, getOutput := patchLogPrintln()
-	defer restore()
+	defer c.Cleanup()
+	getOutput := patchLogPrintln(c)
 	l := logger.New(logger.AddPrefix("my prefix"), strings.ToUpper)
 	l.Print("of the starship enterprise")
 	c.Assert(getOutput(), qt.Equals, "MY PREFIX: OF THE STARSHIP ENTERPRISE\n")
@@ -30,8 +30,8 @@ func TestModifiers(t *testing.T) {
 
 func TestNilModifiers(t *testing.T) {
 	c := qt.New(t)
-	restore, getOutput := patchLogPrintln()
-	defer restore()
+	defer c.Cleanup()
+	getOutput := patchLogPrintln(c)
 	l := logger.New(nil, nil)
 	l.Print("exterminate")
 	c.Assert(getOutput(), qt.Equals, "exterminate\n")
@@ -44,18 +44,13 @@ func TestAddPrefix(t *testing.T) {
 }
 
 // patchLogPrintln patches the logger.LogPrintln variable so that it is
-// possible to collect logs.
-func patchLogPrintln() (restore func(), getOutput func() string) {
-	original := *logger.LogPrintln
+// possible to collect logs. The returned function is used to retrieve logs.
+func patchLogPrintln(c *qt.C) (getOutput func() string) {
 	var output string
-	*logger.LogPrintln = func(v ...interface{}) {
+	c.Patch(logger.LogPrintln, func(v ...interface{}) {
 		output = fmt.Sprintln(v...)
-	}
-	restore = func() {
-		*logger.LogPrintln = original
-	}
-	getOutput = func() string {
+	})
+	return func() string {
 		return output
 	}
-	return restore, getOutput
 }
